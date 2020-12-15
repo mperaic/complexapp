072938681d40d3bd98383b0a181ca6dad10fbdf1
mperaic <mperaic@gmail.com>
2020-12-15 19:53:35 +0100

    begin user validation

--- models/User.js
+++ models/User.js
@@ -1,9 +1,28 @@
 let User = function(data) {
-    this.data = data    
+    this.data = data   
+    this.errors = [] 
+}
+
+User.prototype.validate = function() {
+    if (this.data.username == "") {
+        this.errors.push("You must provide a username.")
+    }
+
+    if (this.data.email == "") {
+        this.errors.push("You must provide a valid email address.")
+    }
+    
+    if (this.data.password == "") {
+        this.errors.push("You must provide a password.")
+    }
 }
 
 User.prototype.register = function() {
-    
+    // step 1 - validate user data
+    this.validate()
+    // step 2 - only if there are no 
+    // validation errors
+    // then save the user data into a database
 }
 
 module.exports = User
