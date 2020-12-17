bc6ecbaa374ed7824f213908e05ed56dd9f4e100
mperaic <mperaic@gmail.com>
2020-12-17 11:08:40 +0100

    add bcrypt and refactor for hashing passwords

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs")
 const usersCollection = require("../db").collection("users")
 const validator = require("validator")
 
@@ -48,8 +49,8 @@ User.prototype.validate = function() {
         this.errors.push("Password must be at least 12 chars.")
     }
 
-    if (this.data.password.length > 100) {
-        this.errors.push("Password cannot exceed 100 chars.")
+    if (this.data.password.length > 50) {
+        this.errors.push("Password cannot exceed 50 chars.")
     }
 
     if (this.data.username.length > 0 && this.data.password.length < 3) {
@@ -65,8 +66,7 @@ User.prototype.login = function() {
     return new Promise((resolve, reject) => {
     this.cleanUp()
     usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
-        if (attemptedUser && attemptedUser.password ==
-            this.data.password) {
+        if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
             resolve("Congrats!")
         } else {
             reject("Invalid username/password.")
@@ -84,6 +84,9 @@ User.prototype.register = function() {
     // step 2 - only if there are no validation errors
     // then save the user data into a database
     if (!this.errors.length) {
+        // hash user password
+        let salt = bcrypt.genSaltSync(10)
+        this.data.password = bcrypt.hashSync(this.data.password, salt)
         usersCollection.insertOne(this.data)
     }
 }
