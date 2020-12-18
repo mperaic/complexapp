eb36f7336c4249c66f56d34a4b1941390fcbb833
mperaic <mperaic@gmail.com>
2020-12-18 15:33:55 +0100

    add author, date, and id to posts

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,10 @@
-let Post = function(data) {
+const postsCollection = require("../db").db().collection("posts")
+const ObjectID = require("mongodb").ObjectID
+
+let Post = function(data, userid) {
     this.data = data
     this.errors = []
+    this.userid = userid
 }
 
 Post.prototype.cleanUp = function() {
@@ -16,7 +20,8 @@ Post.prototype.cleanUp = function() {
     this.data = {
         title: this.data.title.trim(),
         body: this.data.body.trim(),
-        createdDate: new Date()
+        createdDate: new Date(),
+        author: ObjectID(this.userid)
     }
 }
 
@@ -31,7 +36,22 @@ Post.prototype.validate = function() {
 }
 
 Post.prototype.create = function() {
-
+    return new Promise((resolve, reject) => {
+        this.cleanUp()
+        this.validate()
+        if (!this.errors.length) {
+            // save post into database
+            postsCollection.insertOne(this.data).then(
+                () => {resolve()}
+            ).catch(
+                () => {this.errors.push("Please try again later.")
+            reject(this.errors)}
+            )
+            
+        } else {
+            reject(this.errors)
+        }
+    })
 }
 
 module.exports = Post
