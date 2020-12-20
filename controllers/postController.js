17a6adfd6644858e62cc713049435c43dbfe2e9b
mperaic <mperaic@gmail.com>
2020-12-20 11:20:09 +0100

    add code for updating an existing post

--- controllers/postController.js
+++ controllers/postController.js
@@ -22,6 +22,43 @@ exports.viewSingle = async function(req, res) {
     }
 }
 
-exports.viewEditScreen = function(req, res) {
-    res.render("edit-post")
+exports.viewEditScreen = async function(req, res) {
+    try {
+        let post = await Post.findSingleById(req.params.id)
+        res.render("edit-post", {post: post})
+    } catch {
+    res.render("404")
+    }
+}
+
+exports.edit = function(req, res) {
+    let post = new Post(req.body, req.visitorId, req.params.id)
+    post.update().then((status) => {
+        // the post was successfully updated in the 
+        // database or user had permission but there 
+        // were validation errors
+        if (status == "success") {
+            // post was updated in db
+            req.flash("success", "Post is updated!")
+            req.session.save(function() {
+                res.redirect(`post/${req.params.id}/edit`)
+            })
+        } else {
+            post.errors.forEach(function(error) {
+                req.flash("errors", error)
+            })
+            req.session.save(function() {
+                res.redirect(`/post/${req.params.id}/edit`)
+            })
+        }
+    }
+    ).catch(() => {
+        // a post with the requested id does not exist
+        // or if the current visitor is not the owner of 
+        // the requested post
+        req.flash("errors","You do not have permission for that.")
+        req.session.save(function() {
+            res.redirect("/")
+        })
+    })
 }
