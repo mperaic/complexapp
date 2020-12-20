37b968f9bb5dad244f6867ac93277718921f8a5d
mperaic <mperaic@gmail.com>
2020-12-20 19:41:34 +0100

    add code for deleting posts functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -26,17 +26,17 @@ exports.viewSingle = async function(req, res) {
 
 exports.viewEditScreen = async function(req, res) {
     try {
-        let post = await Post.findSingleById(req.params.id)
-        if (post.authorId == req.visitorId) {
-            res.render("edit-post", {post: post})
-        } else {
-            req.flash("errors", "You do not have permission for this.")
-            req.session.save(() => {res.redirect("/")})
-        }
+      let post = await Post.findSingleById(req.params.id, req.visitorId)
+      if (post.isVisitorOwner) {
+        res.render("edit-post", {post: post})
+      } else {
+        req.flash("errors", "You do not have permission to perform that action.")
+        req.session.save(() => res.redirect("/"))
+      }
     } catch {
-    res.render("404")
+      res.render("404")
     }
-}
+  }
 
 exports.edit = function(req, res) {
     let post = new Post(req.body, req.visitorId, req.params.id)
@@ -68,3 +68,14 @@ exports.edit = function(req, res) {
         })
     })
 }
+
+exports.delete = function(req, res) {
+    Post.delete(req.params.id, req.visitorId).then(() => {
+        req.flash("success", "Post deleted!!!")
+        req.session.save(() => {res.redirect(`/profile/${req.session.user.username}`)})
+    }).catch(() => {
+        req.flash("errors", "YOu do not have permission for this!")
+        req.session.save(() => {res.redirect("/")})
+    })
+}
+    
