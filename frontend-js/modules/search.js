04c2f627c54bf1ace8a4e2e7d3bf6ef20f1690cc
mperaic <mperaic@gmail.com>
2020-12-22 16:42:11 +0100

    add frontend js code for search functionality

--- frontend-js/modules/search.js
+++ frontend-js/modules/search.js
@@ -1,5 +1,19 @@
 export default class Search {
+    // 1. select DOM elements and keep track of any useful data
     constructor() {
-        alert("Search JS is Success!!!")
+        this.headerSearchIcon = document.querySelector(".header-search-icon")
+        this.events()
+    }
+
+    // 2. events
+    events() {
+        this.headerSearchIcon.addEventListener("click", (e) => {
+            e.preventDefault()
+            this.openOverlay()
+        })
+    }
+    // 3. methods
+    openOverlay() {
+        alert("openOverlay method just ran!")
     }
 }
