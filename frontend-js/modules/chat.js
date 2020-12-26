9bc9356d20d45bfbb5b27f50d704a64fdcfcbd72
mperaic <mperaic@gmail.com>
2020-12-26 12:25:43 +0100

    add chat functionality

---
+++ frontend-js/modules/chat.js
@@ -0,0 +1,29 @@
+export default class Chat {
+    constructor() {
+        this.chatWrapper = document.querySelector("#chat-wrapper")
+        this.openIcon = document.querySelector(".header-chat-icon")
+        this.injectHTML()
+        this.events()
+    }
+
+    // events
+    events() {
+        this.openIcon.addEventListener("click", () => this.showChat())
+    }
+
+    // methods
+    showChat() {
+        this.chatWrapper.classList.add("chat--visible")
+    }
+
+    injectHTML() {
+        this.chatWrapper.innerHTML = `
+        <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
+        <div id="chat" class="chat-log"></div>
+
+        <form id="chatForm" class="chat-form border-top">
+      <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
+    </form>
+        `
+    }
+}
