c79851de56eaee34661698fbf9e1b33e6922ffb1
mperaic <mperaic@gmail.com>
2020-12-27 10:49:22 +0100

    add code for avatar only in chat field and html for interface

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -24,6 +24,16 @@ export default class Chat {
     // methods
     sendMessageToServer() {
         this.socket.emit("chatMessageFromBrowser", {message: this.chatField.value})
+        this.chatLog.insertAdjacentHTML("beforeend", `
+        <div class="chat-self">
+        <div class="chat-message">
+          <div class="chat-message-inner">
+            ${this.chatField.value}
+          </div>
+        </div>
+        <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+      </div>
+        `)
         this.chatField.value =""
         this.chatField.focus()
     }
@@ -42,6 +52,10 @@ export default class Chat {
 
     openConnection() {
         this.socket = io()
+        this.socket.on("welcome", data => {
+            this.username = data.username
+            this.avatar = data.avatar
+        })
         this.socket.on("chatMessageFromServer", (data) => {
             this.displayMessageFromServer(data)
         })
