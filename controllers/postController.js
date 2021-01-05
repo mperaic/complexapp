ae75824f85a9a3b22f221099ae3a9fb14d5ee20e
mperaic <mperaic@gmail.com>
2021-01-05 16:51:19 +0100

    configure sendgrid

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,6 @@
 const Post = require("../models/Post")
+const sendgrid = require("@sendgrid/mail")
+sendgrid.setApiKey(process.env.SENDGRIDAPIKEY)
 
 exports.viewCreateScreen = function(req, res) {
     res.render("create-post")
@@ -7,6 +9,14 @@ exports.viewCreateScreen = function(req, res) {
 exports.create = function(req, res) {
     let post = new Post(req.body, req.session.user._id)
     post.create().then(function(newId) {
+        sendgrid.send({
+            to: "mperaic@gmail.com",
+            from: "test@test.com",
+            subject: "Congrats on new post!",
+            text: "You did a great job!",
+            html: "Great <strong>great</strong> job!!"
+
+        })
         req.flash("success", "New post created.")
         req.session.save(() => res.redirect(`/post/${newId}`))
     }).catch(function(errors) {
